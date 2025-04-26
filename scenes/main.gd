1f23398778b8116f306e70d773e61d2e4899d046
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2025-04-25 17:10:17 -0700

    Update main.gd
    Remove code handled by BuildingManager

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,36 +2,12 @@ class_name Main
 extends Node2D
 
 @export var tower_packed_scene : PackedScene = null
+@export var building_manager : BuildingManager = null
+
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
-@onready var highlight_tile: HighlightTile = $HighlightTile
-
-const BUILDABLE : String = "buildable"
-const TOWER_GROUP : String = "TOWER_GROUP"
-
-var used_tiles : Array[Vector2i] = []
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_mouse"):
 		var cell_position : Vector2i = tile_map_layer.local_to_map(tile_map_layer.get_local_mouse_position())
+		building_manager.place_tower(cell_position, tower_packed_scene)
 		
-		if used_tiles.has(cell_position):
-			return
-		
-		var cell_data = tile_map_layer.get_cell_tile_data(cell_position).get_custom_data(BUILDABLE)
-		
-		if cell_data == true:
-			used_tiles.append(cell_position)
-			place_tower(cell_position)
-
-
-func place_tower(cell_position : Vector2i) -> void:
-	var new_tower : Node2D = tower_packed_scene.instantiate()
-	add_child(new_tower)
-	
-	#I don't really get why we have to multiply by 64, but it has to do with local_to_map
-	new_tower.position = cell_position * 64
-	
-	new_tower.add_to_group(TOWER_GROUP)
-	
-	print_debug(get_tree().get_nodes_in_group(TOWER_GROUP))
-	
