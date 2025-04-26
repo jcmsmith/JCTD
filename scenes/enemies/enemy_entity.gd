26743a3d9e36f72345723867bddea0f214aa7aab
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2025-04-25 20:34:10 -0700

    Implement pathfinding*
    We have a bug that CoffeeCrow does not, the pathfinding works but the enemy isn't in the correct spot

--- scenes/enemies/enemy_entity.gd
+++ scenes/enemies/enemy_entity.gd
@@ -1,5 +1,27 @@
 class_name EnemyEntity
 extends CharacterBody2D
 
-@export var movement_speed : float = 200.0
+@export var pathfinding_manager : PathfindingManager = null
 @export var target_pos : Marker2D = null
+@export var movement_speed : float = 200.0
+
+var path_array : Array[Vector2i] = []
+
+func _ready() -> void:
+	path_array = pathfinding_manager.get_valid_path(global_position / 64, target_pos.position / 64)
+
+func _process(_delta: float) -> void:
+	get_path_to_position()
+	move_and_slide()
+
+func get_path_to_position() -> void:
+	if len(path_array) > 0:
+		#Vector2, NOT Vector2i
+		var direction : Vector2 = global_position.direction_to(path_array[0])
+		
+		velocity = direction * movement_speed
+		
+		if global_position.distance_to(path_array[0]) <= 10:
+			path_array.remove_at(0)
+	else:
+		velocity = Vector2.ZERO
--- scenes/managers/pathfinding_manager.gd
+++ scenes/managers/pathfinding_manager.gd
@@ -7,18 +7,47 @@ extends Node
 var astar_grid : AStarGrid2D = AStarGrid2D.new()
 var path_array : Array[Vector2i] = []
 
+
 func _ready() -> void:
 	setup_astar_grid()
-	
+
+
 func setup_astar_grid() -> void:
 	astar_grid.region = tile_map_grid.get_used_rect()
 	astar_grid.cell_size = tile_map_grid.tile_set.tile_size
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
 	
 	astar_grid.update()
+
+
+func update_terrain_movement_values() -> void:
+	#done in get_valid_path, but also here just in case
+	path_array.clear()
 	
-	for y in tile_map_grid.get_used_rect().size.y:
-		for x in tile_map_grid.get_used_rect().size.x:
-			var enemy : EnemyEntity = testEnemy.instantiate()
-			enemy.position = tile_map_grid.map_to_local(Vector2i(x,y))
-			add_child(enemy)
+	for i in tile_map_grid.get_used_cells():
+		var tile_movement_cost = get_cell_movement_cost(i)
+		
+		if tile_movement_cost < 10:
+			#Set grid point weight to equal movement cost set in TileSet
+			astar_grid.set_point_weight_scale(i, tile_movement_cost)
+		else:
+			#Set grid point to be unwalkable
+			astar_grid.set_point_solid(i, true)
+
+
+func get_valid_path(start_pos : Vector2i, end_pos : Vector2i) -> Array[Vector2i]:
+	path_array.clear()
+	
+	for point in astar_grid.get_point_path(start_pos, end_pos):
+		var current_point : Vector2i = point
+		
+		#Ensure point is in center of cell
+		current_point += astar_grid.cell_size / 2 as Vector2i
+		
+		path_array.append(current_point)
+	
+	return path_array
+
+
+func get_cell_movement_cost(cell_position : Vector2i) -> int:
+	return tile_map_grid.get_cell_tile_data(cell_position).get_custom_data_by_layer_id(1)
