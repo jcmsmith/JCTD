2d14d0d7c3bee88d76b3d053ad2cb3b885af9440
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2025-04-25 18:34:53 -0700

    Update pathfinding_manager.gd
    Grid test successful

--- scenes/managers/pathfinding_manager.gd
+++ scenes/managers/pathfinding_manager.gd
@@ -2,7 +2,7 @@ class_name PathfindingManager
 extends Node
 
 @export var tile_map_grid : TileMapLayer = null
-@export var 
+@export var testEnemy : PackedScene = null
 
 var astar_grid : AStarGrid2D = AStarGrid2D.new()
 var path_array : Array[Vector2i] = []
@@ -16,3 +16,9 @@ func setup_astar_grid() -> void:
 	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
 	
 	astar_grid.update()
+	
+	for y in tile_map_grid.get_used_rect().size.y:
+		for x in tile_map_grid.get_used_rect().size.x:
+			var enemy : EnemyEntity = testEnemy.instantiate()
+			enemy.position = tile_map_grid.map_to_local(Vector2i(x,y))
+			add_child(enemy)
