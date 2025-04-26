28824e0745bb2a7522c3413de89641d2485c5812
jcmsmith <13570256+jcmsmith@users.noreply.github.com>
2025-04-25 18:04:16 -0700

    Create pathfinding_manager
    Not working yet

---
+++ scenes/managers/pathfinding_manager.gd
@@ -0,0 +1,18 @@
+class_name PathfindingManager
+extends Node
+
+@export var tile_map_grid : TileMapLayer = null
+@export var 
+
+var astar_grid : AStarGrid2D = AStarGrid2D.new()
+var path_array : Array[Vector2i] = []
+
+func _ready() -> void:
+	setup_astar_grid()
+	
+func setup_astar_grid() -> void:
+	astar_grid.region = tile_map_grid.get_used_rect()
+	astar_grid.cell_size = tile_map_grid.tile_set.tile_size
+	astar_grid.diagonal_mode = AStarGrid2D.DIAGONAL_MODE_ONLY_IF_NO_OBSTACLES
+	
+	astar_grid.update()
